abc7c14309e0cdce7e2c2e8930404834d6d9cec3
Andrew Ning <aning97@gmail.com>
2023-03-19 02:56:32 -0400

    Add dockerfile for orbslam image
    Added orbslam docker container based on jahaniam/orbslam3:ubuntu18_melodic_cpu. Updated container script.

--- run_container.bat
+++ run_container.bat
@@ -1,3 +1,3 @@
 REM docker run -td --privileged -e ROS_IP=127.0.0.1 --cap-add=SYS_PTRACE -v "%cd%:/home/dev/ORBSLAM3" -p 7776:22 -p 5900:5900 -p 8000:8000 orbslam
 
-docker run -td --privileged -e ROS_IP=127.0.0.1 --cap-add=SYS_PTRACE -v "%cd%:/home/dev/ORBSLAM3" -p 7776:22 -p 5900:5900 -p 8000:8000 jahaniam/orbslam3 bash
+docker run -td --privileged -e ROS_IP=127.0.0.1 --cap-add=SYS_PTRACE -v "%cd%:/home/dev/ORBSLAM3" -p 7776:22 -p 5900:5900 -p 8000:8000 orbslam3
