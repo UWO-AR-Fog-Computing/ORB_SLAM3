34e093ff7c2a0ac16a6d0e61be3e94a50b440007
Andrew Ning <aning97@gmail.com>
2023-03-20 00:59:58 -0400

    Fix build scripts
    Add missing shebang to scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
-make -j2
+make -j1
--- build_ros.sh
+++ build_ros.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Building ROS nodes"
 
 cd Examples/ROS/ORB_SLAM3
