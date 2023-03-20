f2911aa56ee169800d33c4b654f257030226cdd5
Andrew Ning <aning97@gmail.com>
2023-03-20 03:06:52 -0400

    Fix container script
    Change ROS_PACKAGE_PATH

--- run_container.bat
+++ run_container.bat
@@ -2,4 +2,4 @@ REM docker run -td --privileged -e ROS_IP=127.0.0.1 --cap-add=SYS_PTRACE -v "%cd
 
 docker rm -f orbslam3
 docker run -td --privileged -e ROS_IP=127.0.0.1 --cap-add=SYS_PTRACE --name=orbslam3 -v "%cd%:/home/dev/ORBSLAM3" -p 7776:22 -p 5900:5900 -p 8000:8000 uwofogcomputing/fogservice:orbslam3
-docker exec -it orbslam3 bash -i -c "echo 'ROS_PACKAGE_PATH=/opt/ros/melodic/share:/ORB_SLAM3/Examples/ROS'>>~/.bashrc"
+docker exec -it orbslam3 bash -i -c "echo 'ROS_PACKAGE_PATH=/opt/ros/melodic/share:/home/dev/ORBSLAM3/Examples/ROS'>>~/.bashrc"
