8a36f7cc79ba7870a980d4fba5fd46135fe5f95b
Andrew Ning <aning97@gmail.com>
2023-03-25 23:10:31 -0400

    Add development setup
    Added build scripts, added docker compose setup for development

---
+++ run_euroc_monocular.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pathDatasetEuroc='/home/dev/orbslam/datasets/EuRoC/MH01' #Example, it is necesary to change it by the dataset path
+
+
+#------------------------------------
+# Monocular Examples
+echo "Launching MH01 with Monocular sensor"
+#./Examples/Stereo-Inertial/stereo_inertial_euroc ./Vocabulary/ORBvoc.txt ./Examples/Stereo-Inertial/EuRoC.yaml "$pathDatasetEuroc" ./Examples/Stereo-Inertial/EuRoC_TimeStamps/MH01.txt dataset-MH01_stereoi
+
+#./Examples/Stereo/stereo_euroc ./Vocabulary/ORBvoc.txt ./Examples/Stereo/EuRoC.yaml "$pathDatasetEuroc" ./Examples/Stereo/EuRoC_TimeStamps/MH01.txt dataset-MH01_stereoi
+
+./Examples/Monocular/mono_euroc ./Vocabulary/ORBvoc.txt ./Examples/Monocular/EuRoC.yaml "$pathDatasetEuroc" ./Examples/Monocular/EuRoC_TimeStamps/MH01.txt dataset-MH01_mono
---
+++ setup_build_ros.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'ROS_PACKAGE_PATH=/opt/ros/melodic/share:/home/dev/orbslam/Examples_old/ROS'>>~/.bashrc
+
+# Need to source the bashrc file to make the ROS_PACKAGE_PATH variable available to the current shell
