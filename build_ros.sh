8eaab32ed36f933566617158bf1758a99eae1e39
Andrew Ning <aning97@gmail.com>
2023-03-25 22:37:08 -0400

    Update build setup

--- build_ros.sh
+++ build_ros.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
 echo "Building ROS nodes"
 
-cd Examples/ROS/ORB_SLAM3
+export ROS_PACKAGE_PATH=/opt/ros/melodic/share:/home/dev/orbslam/Examples/ROS/ORB_SLAM3
+
+cd Examples_old/ROS/ORB_SLAM3
 mkdir build
 cd build
-cmake .. -DROS_BUILD_TYPE=Release
-make -j
+cmake .. -DROS_BUILD_TYPE=Debug
+make -j1
